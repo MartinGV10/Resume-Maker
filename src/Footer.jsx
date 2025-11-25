87ec15192b27266ec1a3df4ed0e0f63753908a6e
MartinGV10 <mganen10@gmail.com>
2025-11-24 19:41:48 -0500

    Completed landing page

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer">
+                <h3 className='title'>Re:sumé.io</h3>
+                <h3>Copyright 2025</h3>
+                <h3 className='title'>Resume Builder</h3>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Nav.jsx
@@ -0,0 +1,18 @@
+import './Nav.css'
+
+function Nav() {
+    return (
+        <>
+            <div className="header">
+                <div className="nav-left">
+                    <h2 className='title'>Re:sumé.io</h2>
+                </div>
+                <div className="nav-right">
+                    <h2>Create Resume</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Nav
